5207433bb1f243c0edbdea6a633943e19488e8e6
Erox <44631945+elexisrox@users.noreply.github.com>
2024-02-25 17:14:09 -0700

    Appointments interface

--- src/main/java/app/model/Appointments.java
+++ src/main/java/app/model/Appointments.java
@@ -1,8 +1,23 @@
 package app.model;
 
+import java.time.LocalDateTime;
+
 /**
  * Appointments class provides appointment objects.
  */
 
 public class Appointments {
+
+    int apptID;
+    String apptTitle;
+    String apptDesc;
+    String apptLocation;
+    int apptContactID;
+    String apptType;
+    // TODO: Not sure if correct data types below.
+    LocalDateTime apptStartDate;
+    LocalDateTime apptStartTime;
+    LocalDateTime apptEndDate;
+    LocalDateTime apptEndTime;
+
 }
